11d63f2a4aa373de36a63b2709a9dfad008b2ad9
Jim OKelly <thatrubylove@gmail.com>
2015-09-05 15:08:25 -0500

    moving to ods

--- assets/javascripts/notice.js
+++ assets/javascripts/notice.js
@@ -1,10 +1,2 @@
 $(document).ready(function(){
-  $.get("ad.html").done(function(data) {
-    $(".ad").html(data);
-    console.debug("loaded html from ad.html");
-    $(".schedule-link").on("click", function(e) {
-      var s="I want to make more money and become a successful web developer!";
-      document.location.href = "mailto:assessments@rubymentor.io?subject="+s;
-    });
-  });
 });
